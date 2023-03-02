65a64ae249d131d6a8bd2fea29645b7a7c7236e3
ykanematsu <ykane@hiroshima-cu.ac.jp>
2023-03-02 23:09:14 +0900

    Minor fix

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 setup(
         name="reaxfit",
         author="ykanematsu",
-        version="0.0.7",
+        version="0.0.8",
         license='MIT',
         url='https://github.com/ykanematsu/reaxfit',
         description='parameter fitting for ReaxFF',
