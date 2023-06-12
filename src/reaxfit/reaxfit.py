9ae30450aa095583d74416825b84a095f8227195
ykanematsu <ykane@hiroshima-cu.ac.jp>
2023-06-12 12:07:59 +0900

    Add __main__.py

--- src/reaxfit/reaxfit.py
+++ src/reaxfit/reaxfit.py
@@ -250,31 +250,4 @@ class reaxfit():
       return result
 
 if __name__ =='__main__':
-    if len(sys.argv) <2:
-        reax=reaxfit()
-        result=reax.fit()
-        print("optmized parameters")
-        print(*[f'{p:.5f}' for p in result.x])
-        print("optmized energy and force")
-        print(*[f'{s:.5f}' for s in reax.E])
-        print(*[f'{f:.5f}' for f in reax.F])
-    elif sys.argv[1] == "init":
-        cfile="config.json"
-        if os.path.isfile(cfile): os.replace(cfile,cfile+".bk")
-        with open(cfile,"w") as f:
-            print("create config.json")
-            json.dump(default_option,f,indent=1)
-        sys.exit()
-    elif sys.argv[1] == "check":
-        target_file=default_option["midfile"]
-        if len(sys.argv) > 2: target_file = sys.argv[2]
-        if not os.path.isfile(target_file):
-            print(f"{target_file} not found")
-            sys.exit(1)
-        reax=reaxfit()
-        reax.config(initfile=target_file)
-        pes,fns=reax.reax()
-        print(f"energy and force for {target_file} without optimization (kcal/mol)")
-        print(*[f'{s:.5f}' for s in pes])
-        print(*[f'{f:.5f}' for f in fns])
-
+    pass
