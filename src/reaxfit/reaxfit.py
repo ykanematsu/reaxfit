cc0431f1f6f67aeabcdc54d631dfb3b3351a3211
ykanematsu <ykane@hiroshima-cu.ac.jp>
2024-04-11 12:23:06 +0900

    Fix around multiprocessing for macOS

--- src/reaxfit/reaxfit.py
+++ src/reaxfit/reaxfit.py
@@ -3,9 +3,6 @@ import json,os,sys,re
 import numpy as np
 from scipy.optimize import differential_evolution, minimize
 
-# global parameters are required for multiprocessing
-def wrap_eval(): pass
-def dump_best(): pass
 default_option={
             "refE_file":"refE", # reference energy file
             "refF_file":"refF", # reference force norm file
@@ -103,7 +100,6 @@ class reaxfit():
         f.writelines(l)
 
   def config(self,**kwargs):
-    global dump_best
     if hasattr(kwargs,"cfile"): self.cfile=kwargs["cfile"]
     opt=self.option
     isconfig=os.path.isfile(self.cfile)
@@ -122,8 +118,8 @@ class reaxfit():
         if(optk != kwargs.keys()): 
             print("unrecognized options: ",*(kwargs.keys()-optk))
     self.option=opt
-    midfile=opt["midfile"]
-    stopfile=opt["stopfile"]
+    #midfile=opt["midfile"]
+    #stopfile=opt["stopfile"]
     for k in opt:
         setattr(self,k,opt[k])
     # create scratch directory
@@ -152,20 +148,13 @@ class reaxfit():
     # read template and x0
     with open(self.initfile) as f:
       _template=f.read()
+    with open("0.xyz") as f:
+      _xyz=f.read()
     regex=re.compile("[\{\[][\d.-]+")
     x0=regex.findall(_template)
-    isBound1=[]
-    x1 = 0
-    for x1 in range(len(x0)):
-        if x0[x1].startswith("{"):
-            isBound1.append("True")
-            x0[x1]=x0[x1].strip("{")
-        else:
-            isBound1.append("False")
-            x0[x1]=x0[x1].strip("[")
-        x1+=1
+    isBound1=[x.startswith("{") for x in x0]
     #print(x0)
-    self.x0=[float(x) for x in x0]
+    self.x0=[float(x.strip("{").strip("[")) for x in x0]
     #print(x1)
     self.template = _template
     # change {S
@@ -191,36 +180,32 @@ class reaxfit():
     template=regex.sub("{}",_template)
     self.template=template
     # get elements from templates
-    eles=re.findall(r"\n *(:?[A-Z][a-z]?)",template)
+    #eles=re.findall(r"\n *(:?[A-Z][a-z]?)",template)
+    #idxs=sorted(set(re.findall("\n *\d+ +(\d+) +",_xyz)))
+    eles=re.findall(r"\n *(:?[A-Z][a-z]?)",_xyz)
+    eles=sorted(set(eles),key=eles.index)
+    #idxs=[int(i) for i in idxs]
+    #eles=[eles[i-1] for i in idxs]
     self.elements=" ".join(eles)
     self.refE=refE
     self.refF=refF
-    def callbackF(xk,convergence=False,fout=None):
-      fname=midfile
-      sys.stdout.flush()
-      if fout: fname=fout
-      with open(fname,"w") as f:
-          _x=np.round(xk,5)
-          y=[f"{{{xx}" for xx in _x]
-          f.write(template.format(*y))
-      if os.path.isfile(stopfile):
-          print("optimization will be stopped by the stop file")
-          os.remove(stopfile)
-          return True
-    dump_best=callbackF
+    #dump_best=self.callbackF
     return
-  def set_eval(self,func):
-    global wrap_eval
-    _x0=np.array(self.x0)
-    def wrap_eval(*args,**kwargs):
-      pid=os.getpid()
-      pes,fns=self.reax(*args,pid=pid)
-      output = func(pes,fns,self.refE,self.refF) 
-      if self.harmonic > 0:
-        deltax=(np.array(args[0])-_x0)/(_x0+0.01)
-        output+=deltax@deltax*self.harmonic
-      return output
-    return wrap_eval
+
+  def callbackF(self,xk,convergence=False,fout=None):
+    fname=self.option["midfile"]
+    stopfile=self.option["stopfile"]
+    sys.stdout.flush()
+    if fout: fname=fout
+    with open(fname,"w") as f:
+        _x=np.round(xk,5)
+        y=[f"{{{xx}" for xx in _x]
+        f.write(self.template.format(*y))
+    if os.path.isfile(stopfile):
+        print("optimization will be stopped by the stop file")
+        os.remove(stopfile)
+        return True
+
   def reax(self,xk=None,pid=0):
       x=xk if xk is not None else self.x0
       ffname=f"{self.scrdir}/ffield.reax.{pid}"
@@ -256,38 +241,43 @@ class reaxfit():
       fns=np.array(fns)
       return pes,fns
 
-  def fit(self,func=None):
-      global dump_best
+  def default_func(self,*args,**kwargs):
+    _x0=np.array(self.x0)
+    pid=os.getpid()
+    pes,fns=self.reax(*args,pid=pid)
+    pes0=pes[0]
+    pes-=pes[self.baseIdx] # initial structure is reference by default
+    pes-=self.refE
+    if self.relative_force: fns-=fns[self.baseIdx]
+    fns-=self.refF
+    fns*=self.force_weight
+    fmax=np.abs(fns).max()
+    #return np.sign(pes0)*np.log10(np.abs(pes0)+1) + np.log10(fns@fns+1)
+    output = pes@pes + np.linalg.norm(fns)+np.abs(pes).max()*np.abs(fns).max()+fmax**2
+    if self.harmonic > 0:
+      deltax=(np.array(args[0])-_x0)/(np.abs(_x0)+0.01)
+      output+=deltax@deltax*self.harmonic
+    return output
+
+  def fit(self,myfunc=None):
+      #global dump_best
       if not hasattr(self,"x0"): self.config()
       print("config parameters for fitting")
       json.dump(self.option,sys.stdout)
       print("")
       #refE,refF=self.refE,self.refF
       print(f"initial {len(self.x0)} parameters : {self.x0}")
-      if not func:
-          @self.set_eval
-          def default_eval(pes,fns,refE,refF):
-            pes0=pes[0]
-            pes-=pes[self.baseIdx] # initial structure is reference by default
-            pes-=refE
-            if self.relative_force: fns-=fns[self.baseIdx]
-            fns-=refF
-            fns*=self.force_weight
-            fmax=np.abs(fns).max()
-            #return np.sign(pes0)*np.log10(np.abs(pes0)+1) + np.log10(fns@fns+1)
-            return pes@pes + np.linalg.norm(fns)+np.abs(pes).max()*np.abs(fns).max()+fmax**2
-            #return pes@pes + np.linalg.norm(fns)/len(fns)/10+fns[0]*fns[0]/10
-          func=default_eval
+      func = myfunc if myfunc else self.default_func
       if self.optimizer == "differential_evolution":
         result = differential_evolution(func, self.bounds,workers=self.workers,x0=self.x0,updating='deferred',
-                                  disp=True,maxiter=self.maxiter,tol=self.tol,callback=dump_best,popsize=16,seed=self.seed)
+                                  disp=True,maxiter=self.maxiter,tol=self.tol,callback=self.callbackF,popsize=16,seed=self.seed)
       else:
-        result = minimize(func,x0=self.x0,method=self.optimizer,tol=self.tol,callback=dump_best,jac='3-point') 
+        result = minimize(func,x0=self.x0,method=self.optimizer,tol=self.tol,callback=self.callbackF,jac='3-point') 
       print(result)
-      dump_best(result.x,fout=self.endfile)
+      self.callbackF(result.x,fout=self.endfile)
       self.result=result
       self.E,self.F=self.reax(result.x)
       return result
 
 if __name__ =='__main__':
-    pass
+  pass
